995b8242e646ea4d77af26573871fac6232306c1
Taariq Levack <taariql@gmail.com>
2022-07-27 12:57:10 +0200

    next js

---
+++ components/cards/keplr/constants/constants.js
@@ -0,0 +1,8 @@
+export const DENOM = 'SCRT';
+export const MINIMAL_DENOM = 'uscrt';
+export const AMOUNT_GIVEN = 1000000000;
+export const GRPCWEB_URL = 'https://grpc.pulsar.scrttestnet.com';
+export const RPC_URL = 'https://rpc.pulsar.scrttestnet.com';
+export const LCD_URL = 'https://api.pulsar.scrttestnet.com';
+export const CHAIN_ID = 'pulsar-2';
+export const CHAIN_NAME = 'Secret Testnet';
